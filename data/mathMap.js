a8db7546c3fbf3a18cc0c9d015bfa078f05b0791
hammerstick <steeltermite@lwpm.tech>
2024-12-11 20:50:16 -0800

    license: Add GPLv3 as the license for this program

--- data/mathMap.js
+++ data/mathMap.js
@@ -1,3 +1,14 @@
+/* SPDX-License-Identifier: GPL-3.0-only */
+
+/* This file is part of qa-reports.
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 "use strict";
 
 /** This map is used to allow the user to input math symbols commonly used in machinists' documents */
--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # qa-reports
 
 Basic QA-related reports for machined parts.
+
+## License
+
+Copyright (C) 2024 hammerstick
+
+This program (qa-reports) is licensed under the GNU General Public License v3.
